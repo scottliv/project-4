b8096d6ace2468c87fe44d65760d897d90edcb15
Scott <scottdlivingstone@gmail.com>
2017-11-11 14:57:12 -0800

    start footer styling

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -10,8 +10,35 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				<div class="container footer-container">
+
+					<div class="contact">
+						<h3 class="footer-heading">Contact Info</h3>
+						<p>
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+							<a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+						</p>
+						<p>
+							<i class="fa fa-phone" aria-hidden="true"></i>
+							<a href="tel:778-456-7891">778-456-7891</a>
+						</p>
+						<p class="social-icons">
+							<i class="fa fa-facebook-square" aria-hidden="true"></i>
+							<i class="fa fa-twitter-square" aria-hidden="true"></i>
+							<i class="fa fa-google-plus-square" aria-hidden="true"></i>
+						</p>
+					</div>
+
+					<div class="hours">
+						<h3 class="footer-heading">Business Hours</h3>
+						<p><span class="bold">Monday-Friday:</span> 9am to 5pm</p>
+						<p><span class="bold">Saturday:</span> 10am to 2pm</p>
+						<p><span class="bold">Sunday:</span> Closed</p>
+					</div>
+
+					<div class="footer-image">
+						<img src="./images/inhabitent-logo-text.svg" alt="Inhabitent">
+					</div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
