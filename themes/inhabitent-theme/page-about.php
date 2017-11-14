6c9939393b7890f7fb79c953f4d8cf9d4ce31828
Scott <scottdlivingstone@gmail.com>
2017-11-14 11:25:23 -0800

    add inhabitent functionality plugin for custom post type

---
+++ themes/inhabitent-theme/page-about.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The front page template.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<section class="about-hero">
+			<div class="about-hero-image"></div>
+		</section>
+
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
