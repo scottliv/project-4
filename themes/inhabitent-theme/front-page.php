0be7264f1254b019339d5535295997ed58820221
Scott <scottdlivingstone@gmail.com>
2017-11-13 16:55:22 -0800

    add recent blog posts to home page

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -49,34 +49,40 @@ get_header(); ?>
 
 		<section class="home-journal container centered">
 			<h3>Inhabitent Journal</h3>
+
+			<div class="home-journal-flex-wrapper">
+				
+				<?php $args = array( 'post_type' => 'post', 'posts_per_page' => 3 ); $query = new WP_Query( $args );?>
+
+					<?php if ( $query->have_posts() ) : ?>
+						<?php if ( is_home() && ! is_front_page() ) : ?>
+							<header>
+								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							</header>
+						<?php endif; ?>
+
+						<?php /* Start the Loop */ ?>
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+							<?php get_template_part( 'template-parts/content-home-journal' ); ?>
+
+						<?php endwhile; ?>
+
+						<?php the_posts_navigation(); ?>
+
+					<?php else : ?>
+
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+					<?php endif; ?>
+			</div>
 		</section>
 
 			<section class="home-adventures container centered">
 			<h3>Latest Adventures</h3>
 		</section>
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content' ); ?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+		
+	
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
