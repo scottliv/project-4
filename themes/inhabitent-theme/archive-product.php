c3aeb6f84d31d244b64b07011f1f63a7e3fccd84
Scott <scottdlivingstone@gmail.com>
2017-11-16 11:30:48 -0800

    add business hours and contact info widgets to sidebar

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -10,6 +10,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+				<?php
+				$terms = get_terms( array(
+					'taxonomy' => 'product-type',
+					'hide_empty' => 0,
+				));
+				if ( ! empty( $terms ) ** ! is_wp_error( $terms)):
+				?>
+				<div class="flex-wrapper container">
+					<?php foreach ($terms as $term) : ?>
+						<div class="product-type-block-wrapper shop-category">	
+							<p><a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?> </a> </p>
+						</div>
+					<?php endforeach; ?>
+				</div>
+				<?php endif ?>
+
 			
 				<?php $args = array( 'post_type' => 'product', 'posts_per_page' => -1 ); $query = new WP_Query( $args );?>
 
