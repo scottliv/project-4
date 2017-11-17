088adff4356adae3a4dc4327cf628264f01a887e
Scott <scottdlivingstone@gmail.com>
2017-11-17 12:54:59 -0800

    add logo to front page hero

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -10,7 +10,9 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<section class="home-hero">
-			<div class="home-hero-image"></div>
+			<div class="home-hero-image">
+				<img src="<?php echo get_template_directory_uri() .'/images/inhabitent-logo-full.svg'?>" alt="" class="hero-logo">
+			</div>
 		</section>
 
 		<section class="home-shop container centered">
