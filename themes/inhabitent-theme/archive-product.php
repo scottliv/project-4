b1879ff6c092b5e950f51b9d2edb8f5c4f864395
Scott <scottdlivingstone@gmail.com>
2017-11-18 13:59:05 -0800

    style comments

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="product-type-title-wrapper">
+			<div class="product-type-title-wrapper container">
 				<h1 class="page-title centered">Shop</h1>
 					<?php
 					$terms = get_terms( array(
