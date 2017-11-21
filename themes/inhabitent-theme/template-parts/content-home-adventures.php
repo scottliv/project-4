1088ef0a150a1c7731240569b555131c208884fd
Scott <scottdlivingstone@gmail.com>
2017-11-21 14:00:59 -0800

    attach background image to grid containers in order to fix image sizing issue in home page adventures

--- themes/inhabitent-theme/template-parts/content-home-adventures.php
+++ themes/inhabitent-theme/template-parts/content-home-adventures.php
@@ -7,25 +7,24 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<?php if ( has_post_thumbnail() ) : ?>
+  <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+<?php endif; ?>
 
-     <div class="home-adventures-container">
-      <?php if ( has_post_thumbnail() ) : ?>
-        <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-      
-        <div class="home-adventures-image-container" style="
-          background: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100%), url('<?php echo $backgroundImg[0] ?>');
-         background-size: cover;
-         background-repeat: no-repeat;
-         background-position: center;
-         ">
+  <div class="home-adventures-image-container" style="
+        background: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100%), url('<?php echo $backgroundImg[0] ?>');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        ">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		    <?php echo sprintf('<a href="%s">', esc_url( get_permalink() ) ) . '<button class="btn-white">Read More</button></a>';?>
+    	<header class="entry-header">
+        <div class="home-adventures-container">
+		      <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+          <?php echo sprintf('<a href="%s">', esc_url( get_permalink() ) ) . '<button class="btn-white">Read More</button></a>';?>
+        </div>
+      </header><!-- .entry-header -->
 
-      <?php endif; ?>
-      </div>
-    </header><!-- .entry-header -->
-
-</article><!-- #post-## -->
+    </article><!-- #post-## -->
+  </div>
